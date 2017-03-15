1ef65e57ecfe1f24e0210eff2411ad846a2bb493
Bruce Wayne <admin@bige0.com>
2017-03-16 02:03:33 +0800

    A

--- 7-欧拉函数/UVa 10299.cpp
+++ 7-欧拉函数/UVa 10299.cpp
@@ -1,147 +1,53 @@
 #include <iostream>
-#include <vector>
-#include <set>
 #include <new>
-#include <cmath>
-#include <algorithm>
-#include <memory.h>
+
 using namespace std;
 typedef uintmax_t ull;
 
-class Primes
-{
+class Euler {
 private:
-	set<ull> Prime;
-	bool *isPrime = NULL;
-	ull *vecPrime = NULL;
-	void release();
+
 public:
-	Primes(ull);
-	~Primes();
-	void MakePrime(ull);//膜改筛法素数生成器
-	ull Number();//返回生成的素数个数
-	ull Number(ull n);//返回<=n的素数的个数
-	ull at(ull n);//返回第n个素数，错误返回0
-	bool isPrimeNumer(ull);//判断是否是素数
-	ull find(ull);//返回n是第几个素数，错误返回0
-	vector<pair<ull, ull> >factor(ull n);//将n分解质因数
-	ull Euler(ull n);//返回欧拉函数
+	ull _Euler(ull n);
+	ull* MakeEuler(ull N);
 };
-Primes::Primes(ull N) {
-	N = min((ull)Prime.max_size(), N);
-	MakePrime(N);
-}
-Primes::~Primes() {
-	release();
-}
-void Primes::MakePrime(ull N)//膜改快速线性筛法
-{
-	release();
-	ull m = N;
-	--N;
-	N /= 2;
-	ull sn = (ull)(sqrt(2 * N) / 2);
-	vector<bool> p(N, 1);
-	for (ull i = 0; i < sn; i++) {
-		if (p[i])
-		{
-			for (ull j = i*(2 * i + 6) + 3; j<N; j += 2 * i + 3)
-				p[j] = false;
-		}
-	}
-	set<ull>().swap(Prime);
-	isPrime = new bool[m + 1];
-	memset(isPrime, 0, sizeof(bool)*(m + 1));
-	Prime.insert(2);
-	isPrime[2] = true;
-	for (ull i = 0; i<N; ++i) {
-		if (p[i]) {
-			Prime.insert(i + i + 3);
-			isPrime[i + i + 3] = true;
-		}
-	}
-	vecPrime = new ull[Prime.size()];
-	vecPrime[0] = 2;
-	for (ull i = 0, j = 0; i<N; ++i) {
-		if (p[i]) {
-			vecPrime[++j] = i + i + 3;
-		}
-	}
-	return;
-}
 
-void Primes::release() {
-	if (isPrime != NULL)
-		delete[] isPrime;
-	if (vecPrime != NULL)
-		delete[] vecPrime;
-}
-ull Primes::Number()
+ull Euler::_Euler(ull n)
 {
-	return Prime.size();
-}
-
-ull Primes::at(ull n)
-{
-	if (n < Number() && n>0)
-		return vecPrime[n - 1];
-	else
-		return 0;
-}
-ull Primes::Number(ull n)
-{
-	/*auto it = Prime.begin();
-	advance(it, n);*/
-	return distance(Prime.begin(), Prime.upper_bound(n));;
-}
-bool Primes::isPrimeNumer(ull num)
-{
-	set<ull>::iterator it = Prime.find(num);
-	if (it == Prime.end())
-		return false;
-	return true;
-}
-ull Primes::find(ull num)
-{
-	set<ull>::iterator it = Prime.find(num);
-	if (it == Prime.end())
-		return 0;
-	return distance(Prime.begin(), Prime.upper_bound(num));
-}
-vector<pair<ull, ull>> Primes::factor(ull n)
-{
-	ull temp, i, now;
-	vector<pair<ull, ull>> a;
-	temp = Number((ull)(sqrt(n)));
-	now = n;
-	for (i = 0; now > 1 && i < temp; ++i)
-		if (now%vecPrime[i] == 0) {
-			a.push_back(make_pair(vecPrime[i], 0));
-			while (now%vecPrime[i] == 0) {
-				++get<1>(*(a.end() - 1));
-				now /= vecPrime[i];
-			}
+	ull a = n;
+	for (int i = 2; i*i <= a; i++) {
+		if (a%i == 0) {
+			n = n/i*(i - 1);
+			while (a%i == 0) a /= i;
 		}
-	if (now != 1)
-		a.push_back(make_pair(now, 1));
-	return a;
-}
-ull Primes::Euler(ull n)
-{
-	if (n == 1)
-		return 0;
-	vector<pair<ull, ull> > f = factor(n);
-	for (int i = 0; i < f.size(); ++i)
-		n = n / f[i].first * (f[i].first - 1);
+	}
+	if (a>1) n = n / a*(a - 1);
 	return n;
+}
+ull* Euler::MakeEuler(ull N)
+{
+	ull *phi = new ull[N + 1];
+	int i, j;
+	for (i = 1; i <= N; i++)
+		phi[i] = i;
+	for (i = 2; i <= N; i += 2)
+		phi[i] /= 2;
+	for (i = 3; i <= N; i += 2)
+		if (phi[i] == i) {
+			for (j = i; j <= N; j += i)
+				phi[j] = phi[j] / i * (i - 1);
+		}
+	return phi;
 }
 int main() {
 	ios::sync_with_stdio(false);
 	int n;
-	Primes p((ull)sqrt(1000000000));
-	vector<pair<ull, ull> > f;
+	Euler e;
 	while (cin >> n && n) {
-		cout << p.Euler(n) << endl;
+		if (n == 1)
+			cout << 0 << endl;
+		else
+			cout << e._Euler(n) << endl;
 	}
 	//system("pause");
 	return 0;
